c918dcba61d94f4a6e80997b5c6ff7fd6a357054
stefanicjuraj <jurajstefanic@outlook.com>
2025-01-08 14:26:43 +0100

    feat: add allergies management

--- web/app/allergies/page.tsx
+++ web/app/allergies/page.tsx
@@ -1,3 +1,408 @@
-export default function Allergies() {
-  return <></>;
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  Timestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import useAuth from "../../auth/useAuth";
+import Login from "../../components/Login";
+import Navbar from "../../components/Navbar";
+import AddAllergyModal from "../../components/AddAllergy";
+
+interface Allergy {
+  id: string;
+  allergy: string;
+  subType?: string | null;
+  severity: string;
+  symptoms: string[];
+  uid: string;
+  createdAt: Timestamp;
 }
+
+const EditAllergyModal: React.FC<{
+  isOpen: boolean;
+  allergy: Allergy | null;
+  onClose: () => void;
+}> = ({ isOpen, allergy, onClose }) => {
+  const [editedAllergy, setEditedAllergy] = useState<Allergy | null>(null);
+
+  useEffect(() => {
+    if (allergy) {
+      setEditedAllergy(allergy);
+    }
+  }, [allergy]);
+
+  const handleInputChange = (key: keyof Allergy, value: string) => {
+    if (!editedAllergy) return;
+    setEditedAllergy({ ...editedAllergy, [key]: value });
+  };
+
+  const handleSeverityChange = (value: string) => {
+    if (!editedAllergy) return;
+    setEditedAllergy({ ...editedAllergy, severity: value });
+  };
+
+  const handleSymptomToggle = (symptom: string) => {
+    if (!editedAllergy) return;
+    const currentSymptoms = editedAllergy.symptoms || [];
+    if (currentSymptoms.includes(symptom)) {
+      setEditedAllergy({
+        ...editedAllergy,
+        symptoms: currentSymptoms.filter((s) => s !== symptom),
+      });
+    } else {
+      setEditedAllergy({
+        ...editedAllergy,
+        symptoms: [...currentSymptoms, symptom],
+      });
+    }
+  };
+
+  const handleSave = async () => {
+    if (!editedAllergy) return;
+
+    try {
+      const docRef = doc(db, "allergies", editedAllergy.id);
+
+      const updatedData = {
+        allergy: editedAllergy.allergy || "",
+        severity: editedAllergy.severity || "",
+        symptoms: editedAllergy.symptoms || [],
+      };
+
+      await updateDoc(docRef, updatedData);
+
+      onClose();
+    } catch (error) {
+      console.error("Failed to update allergy:", error);
+      alert("Failed to update allergy. Please try again.");
+    }
+  };
+
+  if (!isOpen || !allergy || !editedAllergy) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-2">
+      <div className="bg-white p-8 rounded-lg shadow-lg sm:w-2/6 w-full relative">
+        <button
+          onClick={onClose}
+          className="absolute top-5 right-4 text-sm text-black px-4 py-2 bg-gray-100 rounded focus:outline-none focus:ring-0"
+          aria-label="Close"
+        >
+          x
+        </button>
+
+        <h2 className="text-2xl font-bold mb-8">Edit Allergy</h2>
+        <div>
+          <label className="block mb-2 font-bold">Allergy:</label>
+          <input
+            type="text"
+            value={editedAllergy.allergy || ""}
+            onChange={(e) => handleInputChange("allergy", e.target.value)}
+            className="w-full p-3 border border-black border-r-4 border-b-4 rounded mb-4 focus:outline-none focus:ring-0 focus:border-black"
+          />
+
+          {
+            /* Sub-Type
+          <label className="block mb-2 font-bold">Sub-Type:</label>
+          <input
+            type="text"
+            value={editedAllergy.subType || ""}
+            onChange={(e) => handleInputChange("subType", e.target.value)}
+            className="w-full p-3 border border-black border-r-4 border-b-4 rounded mb-4 focus:outline-none focus:ring-0 focus:border-black"
+          /> */
+          }
+
+          <label className="block mb-2 font-bold">Severity:</label>
+          <select
+            value={editedAllergy.severity || ""}
+            onChange={(e) => handleSeverityChange(e.target.value)}
+            className="w-full p-3 border border-black border-r-4 border-b-4 rounded mb-4 focus:outline-none focus:ring-0 focus:border-black"
+          >
+            <option value="">Select Severity</option>
+            <option value="Mild">Mild</option>
+            <option value="Moderate">Moderate</option>
+            <option value="Severe">Severe</option>
+            <option value="Extreme">Extreme</option>
+          </select>
+
+          <label className="block mb-2 font-bold">Symptoms:</label>
+          <div className="grid grid-cols-2 gap-4 max-h-60 overflow-y-auto">
+            {[
+              "Sneezing",
+              "Itchy Eyes",
+              "Runny Nose",
+              "Cough",
+              "Difficulty Breathing",
+              "Hives",
+              "Swelling",
+              "Anaphylaxis",
+            ].map((symptom) => (
+              <div
+                key={symptom}
+                onClick={() => handleSymptomToggle(symptom)}
+                className={`p-4 border rounded-lg text-center cursor-pointer border-r-4 border-b-4 ${
+                  editedAllergy.symptoms.includes(symptom)
+                    ? "border-black bg-blue-300"
+                    : "border-black"
+                }`}
+              >
+                {symptom}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex justify-start space-x-4 mt-6">
+          <button
+            onClick={handleSave}
+            className="px-8 py-4 text-lg bg-blue-300 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg my-4"
+          >
+            Save
+          </button>
+          <button
+            onClick={onClose}
+            className="px-8 py-4 text-lg bg-gray-100 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg my-4"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Allergies = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [allergies, setAllergies] = useState<Allergy[]>([]);
+  const [selectedAllergy, setSelectedAllergy] = useState<Allergy | null>(null);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const { user } = useAuth();
+
+  const handleOpenAddModal = () => setIsAddModalOpen(true);
+  const handleCloseAddModal = () => setIsAddModalOpen(false);
+
+  const handleEditModalOpen = (allergy: Allergy) => {
+    setSelectedAllergy(allergy);
+    setIsEditModalOpen(true);
+  };
+
+  const handleEditModalClose = () => {
+    setSelectedAllergy(null);
+    setIsEditModalOpen(false);
+  };
+
+  const handleDeleteModalOpen = (allergy: Allergy) => {
+    setSelectedAllergy(allergy);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeleteModalClose = () => {
+    setSelectedAllergy(null);
+    setIsDeleteModalOpen(false);
+  };
+
+  const handleDelete = async () => {
+    if (!selectedAllergy) return;
+
+    try {
+      const docRef = doc(db, "allergies", selectedAllergy.id);
+      await deleteDoc(docRef);
+      setSelectedAllergy(null);
+      setIsDeleteModalOpen(false);
+    } catch (error) {
+      console.error("Failed to delete allergy:", error);
+      alert("Failed to delete allergy. Please try again.");
+    }
+  };
+
+  const allergyTypeEmojis: { [key: string]: string } = {
+    Dust: "🌫️",
+    Peanuts: "🥜",
+    "Fish Products": "🐟",
+    "Pet Dander": "🐾",
+    "Insect Stings": "🐝",
+    Medication: "💊",
+    Other: "📋",
+  };
+
+  const allergyTypeColors: { [key: string]: string } = {
+    Pollen: "bg-blue-100",
+    Dust: "bg-gray-100",
+    Peanuts: "bg-yellow-100",
+    "Fish Products": "bg-teal-100",
+    "Pet Dander": "bg-pink-100",
+    "Insect Stings": "bg-orange-100",
+    Medication: "bg-green-100",
+    Other: "bg-purple-100",
+  };
+
+  useEffect(() => {
+    if (!user) return;
+
+    const allergiesCollection = collection(db, "allergies");
+    const q = query(allergiesCollection, where("uid", "==", user.uid));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const allergyList = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Allergy[];
+      setAllergies(allergyList);
+    });
+
+    return () => unsubscribe();
+  }, [user]);
+
+  if (!user) {
+    return (
+      <div>
+        <Navbar />
+        <div className="p-4 max-w-screen-lg mx-auto">
+          <h1 className="text-5xl font-black">My Allergies</h1>
+          <p className="text-black mt-8">
+            Please log in to view your allergies.
+          </p>
+          <Login />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="p-4 max-w-screen-lg mx-auto mb-40">
+        <h1 className="text-5xl font-bold">My Allergies</h1>
+        <button
+          onClick={handleOpenAddModal}
+          className="px-7 py-3 text-lg bg-blue-300 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg my-8"
+        >
+          Add Allergy
+        </button>
+
+        <AddAllergyModal
+          isOpen={isAddModalOpen}
+          onClose={handleCloseAddModal}
+        />
+
+        <div className="flex flex-col space-y-4 animation delay-1">
+          {allergies.length === 0
+            ? <p className="text-gray-600">No allergies added yet.</p>
+            : (
+              allergies.map((allergy) => (
+                <div
+                  key={allergy.id}
+                  className={`${
+                    allergyTypeColors[allergy.allergy] || "bg-blue-50"
+                  } sm:p-6 p-4 rounded-lg border border-black border-b-4 border-r-4 shadow-md hover:shadow-lg flex items-center`}
+                >
+                  <div className="sm:text-4xl text-3xl sm:mr-6 mr-4 flex-shrink-0 rounded-xl p-3">
+                    {allergyTypeEmojis[allergy.allergy] || "📋"}
+                  </div>
+                  <div className="flex-1">
+                    <div className="flex justify-between items-center">
+                      <div>
+                        <h2 className="sm:text-xl text-md font-bold mb-1">
+                          {allergy.allergy}
+                        </h2>
+                        {allergy.subType && (
+                          <p className="mb-1 text-sm font-semibold">
+                            {allergy.subType}
+                          </p>
+                        )}
+                        <p className="mb-1 text-sm font-semibold">
+                          Severity: {""}
+                          <span className="font-normal">
+                            {allergy.severity}
+                          </span>
+                        </p>
+                      </div>
+                      <div className="flex sm:space-x-4 space-x-2">
+                        <button
+                          onClick={() => handleEditModalOpen(allergy)}
+                          aria-label="Edit Allergy"
+                        >
+                          <img
+                            src="/icons/edit.svg"
+                            className="sm:h-10 sm:w-10 w-8 h-8 p-1"
+                            alt="Edit Allergy"
+                          />
+                        </button>
+                        <button
+                          onClick={() => handleDeleteModalOpen(allergy)}
+                          aria-label="Delete Allergy"
+                        >
+                          <img
+                            src="/icons/delete.svg"
+                            className="sm:h-10 sm:w-10 w-8 h-8 p-1"
+                            alt="Delete Allergy"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                    {allergy.symptoms && allergy.symptoms.length > 0 && (
+                      <div className="mt-0">
+                        <h3 className="font-bold text-sm">
+                          Symptoms:{" "}
+                          <span className="font-normal">
+                            {allergy.symptoms.join(", ")}
+                          </span>
+                        </h3>
+                      </div>
+                    )}
+                    <p className="text-sm mt-2">
+                      {allergy.createdAt.toDate().toLocaleDateString()}
+                    </p>
+                  </div>
+                </div>
+              ))
+            )}
+        </div>
+
+        <EditAllergyModal
+          isOpen={isEditModalOpen}
+          allergy={selectedAllergy}
+          onClose={handleEditModalClose}
+        />
+
+        {isDeleteModalOpen && selectedAllergy && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-2">
+            <div className="bg-white p-6 rounded-lg shadow-lg sm:w-1/4 w-full">
+              <h2 className="text-xl font-bold mb-4">Confirm Deletion</h2>
+              <p>
+                Are you sure you want to delete the allergy "{selectedAllergy
+                  .allergy}"?
+              </p>
+              <div className="flex justify-center mt-6 space-x-4">
+                <button
+                  onClick={handleDeleteModalClose}
+                  className="px-7 py-3 text-lg bg-gray-100 hover:bg-gray-200 font-bold border-b-4 border-black border-r-4 rounded hover:shadow-lg"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleDelete}
+                  className="px-7 py-3 text-lg bg-red-400 hover:bg-red-500 font-bold border-b-4 border-black border-r-4 rounded hover:shadow-lg"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Allergies;
---
+++ web/components/AddAllergy.tsx
@@ -0,0 +1,326 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+import useAuth from "../auth/useAuth";
+
+interface AddAllergyModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const AddAllergyModal: React.FC<AddAllergyModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  const [selectedAllergy, setSelectedAllergy] = useState("");
+  const [customAllergy, setCustomAllergy] = useState("");
+  const [selectedSubType, setSelectedSubType] = useState("");
+  const [customSubType, setCustomSubType] = useState("");
+  const [selectedSeverity, setSelectedSeverity] = useState("");
+  const [selectedSymptoms, setSelectedSymptoms] = useState<string[]>([]);
+  const [step, setStep] = useState(0);
+  const { user } = useAuth();
+
+  const subTypeOptionsMap: { [key: string]: string[] } = {
+    "Pet Dander": ["Cat", "Dog", "Bird", "Other"],
+    Medication: ["Ibuprofen", "Penicillin", "Aspirin", "Other"],
+    "Insect Stings": ["Bee", "Wasp", "Hornet", "Other"],
+  };
+
+  const allergyOptions = [
+    "Pollen",
+    "Peanuts",
+    "Fish Products",
+    "Pet Dander",
+    "Insect Stings",
+    "Medication",
+  ];
+
+  const severityOptions = ["Mild", "Moderate", "Severe", "Extreme"];
+
+  const symptomOptions = [
+    "Sneezing",
+    "Itchy Eyes",
+    "Runny Nose",
+    "Cough",
+    "Difficulty Breathing",
+    "Hives",
+    "Swelling",
+    "Anaphylaxis",
+  ];
+
+  const requiresSubType = selectedAllergy && subTypeOptionsMap[selectedAllergy];
+
+  const totalSteps = requiresSubType ? 4 : 3;
+
+  const handleAddRecord = async () => {
+    if (!user) {
+      alert("You must be logged in to add an allergy.");
+      return;
+    }
+
+    try {
+      const allergiesCollection = collection(db, "allergies");
+      await addDoc(allergiesCollection, {
+        allergy: selectedAllergy || customAllergy,
+        subType: selectedSubType || customSubType || null,
+        severity: selectedSeverity,
+        symptoms: selectedSymptoms,
+        uid: user.uid,
+        createdAt: new Date(),
+      });
+
+      setSelectedAllergy("");
+      setCustomAllergy("");
+      setSelectedSubType("");
+      setCustomSubType("");
+      setSelectedSeverity("");
+      setSelectedSymptoms([]);
+      setStep(0);
+      onClose();
+    } catch (error) {
+      console.error("Error adding allergy:", error);
+      alert("There was an error adding your allergy. Please try again.");
+    }
+  };
+
+  const handleNextStep = () => {
+    if (step === 0) {
+      if (!selectedAllergy && !customAllergy.trim()) {
+        alert("Please select an allergy or enter a custom one.");
+        return;
+      }
+    } else if (step === 1 && requiresSubType) {
+      if (!selectedSubType && !customSubType.trim()) {
+        alert("Please select a sub-type or enter a custom one.");
+        return;
+      }
+    } else if (step === (requiresSubType ? 2 : 1)) {
+      if (!selectedSeverity) {
+        alert("Please select the severity.");
+        return;
+      }
+    }
+
+    setStep((prevStep) => {
+      const nextStep = prevStep + 1;
+      if (
+        requiresSubType && prevStep === 0 && !subTypeOptionsMap[selectedAllergy]
+      ) {
+        return nextStep + 1;
+      }
+      return Math.min(nextStep, totalSteps - 1);
+    });
+  };
+
+  const handlePreviousStep = () => {
+    setStep((prevStep) => {
+      let prev = prevStep - 1;
+      if (
+        requiresSubType && prevStep === 3 && !subTypeOptionsMap[selectedAllergy]
+      ) {
+        prev = prev - 1;
+      }
+      return Math.max(prev, 0);
+    });
+  };
+
+  const handleCancel = () => {
+    setSelectedAllergy("");
+    setCustomAllergy("");
+    setSelectedSubType("");
+    setCustomSubType("");
+    setSelectedSeverity("");
+    setSelectedSymptoms([]);
+    setStep(0);
+    onClose();
+  };
+
+  useEffect(() => {
+    setSelectedSubType("");
+    setCustomSubType("");
+  }, [selectedAllergy]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-md flex justify-center items-center z-50 p-2">
+      <div className="bg-white p-8 rounded-lg shadow-lg sm:w-2/6 w-full relative">
+        <button
+          onClick={handleCancel}
+          className="absolute top-7 right-4 text-sm text-black px-4 py-2 bg-gray-100 rounded focus:outline-none focus:ring-0"
+          aria-label="Close"
+        >
+          x
+        </button>
+
+        {step === 0 && (
+          <div>
+            <h2 className="block text-2xl font-bold mb-4">Select Allergy</h2>
+            <div className="grid grid-cols-2 gap-4 max-h-60 overflow-y-auto">
+              {allergyOptions.map((option) => (
+                <div
+                  key={option}
+                  onClick={() => {
+                    setSelectedAllergy(option);
+                    setCustomAllergy("");
+                  }}
+                  className={`p-4 border rounded-lg text-center cursor-pointer border-r-4 border-b-4 ${
+                    selectedAllergy === option
+                      ? "border-black bg-blue-300"
+                      : "border-black"
+                  }`}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                placeholder="Or enter a custom allergy"
+                value={customAllergy}
+                onChange={(e) => {
+                  setCustomAllergy(e.target.value);
+                  setSelectedAllergy("");
+                }}
+                maxLength={50}
+                className="w-full p-4 border border-black rounded focus:outline-none focus:ring-0 focus:border-black"
+              />
+            </div>
+          </div>
+        )}
+
+        {step === 1 && requiresSubType && (
+          <div>
+            <h2 className="block text-2xl font-bold mb-4">Select Sub-Type</h2>
+            <div className="grid grid-cols-2 gap-4 max-h-60 overflow-y-auto">
+              {subTypeOptionsMap[selectedAllergy].map((option) => (
+                <div
+                  key={option}
+                  onClick={() => {
+                    setSelectedSubType(option);
+                    setCustomSubType("");
+                  }}
+                  className={`p-4 border rounded-lg text-center cursor-pointer border-r-4 border-b-4 ${
+                    selectedSubType === option
+                      ? "border-black bg-blue-300"
+                      : "border-black"
+                  }`}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+            <div className="mt-4">
+              <input
+                type="text"
+                placeholder="Or enter a custom sub-type"
+                value={customSubType}
+                onChange={(e) => {
+                  setCustomSubType(e.target.value);
+                  setSelectedSubType("");
+                }}
+                maxLength={50}
+                className="w-full p-4 border border-black rounded focus:outline-none focus:ring-0 focus:border-black"
+              />
+            </div>
+          </div>
+        )}
+
+        {(step === (requiresSubType ? 2 : 1)) && (
+          <div>
+            <h2 className="block text-2xl font-bold mb-4">Select Severity</h2>
+            <div className="grid grid-cols-2 gap-4">
+              {severityOptions.map((option) => (
+                <div
+                  key={option}
+                  onClick={() => setSelectedSeverity(option)}
+                  className={`p-4 border rounded-lg text-center cursor-pointer border-r-4 border-b-4 ${
+                    selectedSeverity === option
+                      ? "border-black bg-blue-300"
+                      : "border-black"
+                  }`}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {(step === (requiresSubType ? 3 : 2)) && (
+          <div>
+            <h2 className="block text-2xl font-bold mb-4">Select Symptoms</h2>
+            <div className="grid grid-cols-2 gap-4 max-h-60 overflow-y-auto">
+              {symptomOptions.map((option) => (
+                <div
+                  key={option}
+                  onClick={() => {
+                    if (selectedSymptoms.includes(option)) {
+                      setSelectedSymptoms(
+                        selectedSymptoms.filter((sym) => sym !== option),
+                      );
+                    } else {
+                      setSelectedSymptoms([...selectedSymptoms, option]);
+                    }
+                  }}
+                  className={`p-4 border rounded-lg text-center cursor-pointer border-r-4 border-b-4 ${
+                    selectedSymptoms.includes(option)
+                      ? "border-black bg-blue-300"
+                      : "border-black"
+                  }`}
+                >
+                  {option}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        <div className="flex justify-between mt-6">
+          {step > 0 && (
+            <button
+              onClick={handlePreviousStep}
+              className="px-4 py-2 text-lg bg-gray-100 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg"
+            >
+              ⬅ Previous
+            </button>
+          )}
+          {step < totalSteps - 1
+            ? (
+              <button
+                onClick={handleNextStep}
+                className="px-8 py-2 text-lg bg-blue-300 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg"
+              >
+                Next ⮕
+              </button>
+            )
+            : (
+              <button
+                onClick={handleAddRecord}
+                className="px-8 py-2 text-lg bg-green-300 font-bold border border-b-4 border-black border-r-4 rounded hover:shadow-lg"
+              >
+                Submit
+              </button>
+            )}
+        </div>
+
+        <div className="flex items-center text-xs mt-4">
+          <span className="font-bold">{step + 1} / {totalSteps}</span>
+          <div className="w-full bg-gray-200 h-2 rounded-full ml-4">
+            <div
+              className="h-2 bg-blue-300 rounded-full"
+              style={{ width: `${((step + 1) / totalSteps) * 100}%` }}
+            >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddAllergyModal;
